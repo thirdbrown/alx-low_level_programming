dd94b361bece749676f0a12e84aba29ad7e2d927
thirdbrown <brownidyllcreation@gmail.com>
2022-10-16 14:08:21 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints a dtring using fputs'
+ * Return: Always 0 (Success)
+ */
 
-int main(void) {puts('Programming is like building a multilingual puzzle\n);
-	return (0);}
+int main(void)
+{
+	fputs("\"Programming is like building a multilingual puzzle\n, stdout);
+	return (0);
