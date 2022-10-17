190d4bcfc36e4745ff240932366c84dc171b3385
thirdbrown <brownidyllcreation@gmail.com>
2022-10-17 14:01:30 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print number 0-9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
