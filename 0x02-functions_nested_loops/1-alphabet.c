3947907f75eb16f1e8073803543ee39626fa8b15
thirdbrown <brownidyllcreation@gmail.com>
2022-10-18 14:37:45 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print _alphabet - print the alphabeth in lower case
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
