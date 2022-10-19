92eac5b1148580702d3431eb0908d22b535709e2
thirdbrown <brownidyllcreation@gmail.com>
2022-10-19 08:55:39 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,6 +10,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
+	}
 
 		for (mult = 1; mult <= 9; mult++)
 		{
@@ -22,8 +23,8 @@ void times_table(void)
 				_putchar (' ');
 			else
 				_putchar((prod / 10) + '0');
+
 				_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
-	}
 }
