9d86fc6aa539303d52fb5b83d7664aa7fa309b61
thirdbrown <brownidyllcreation@gmail.com>
2022-10-20 03:41:01 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,15 +5,15 @@
  * left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of month
- *  @year: year
- *  Return: void
+ * @year: year
+ * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
-		if (month > 2 && day >= 60)
+		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
