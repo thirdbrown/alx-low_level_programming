04494b369dd18f0502b49da83c1047a363148991
thirdbrown <brownidyllcreation@gmail.com>
2022-11-01 06:20:18 -0700

    memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: memory area
+ * @b: bytes
+ * @n: number of bytes
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n-- > 0)
+		*ptr++ = b;
+}
